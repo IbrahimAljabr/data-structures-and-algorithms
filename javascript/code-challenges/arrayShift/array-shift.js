291d4ec662de0472c196ceef10866cb97d6c3b93
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-03-30 01:27:01 +0300

    readMe

--- javascript/code-challenges/arrayShift/array-shift.js
+++ javascript/code-challenges/arrayShift/array-shift.js
@@ -1,28 +1,28 @@
+    
+const express = require('express');
 
+const app = express();
 
-let arr = [1,2,3,4,5,6,7,8,9,0,];
-
+let arr = [0,1,2,3,4,5,6,7,8,9];
 let num = 99;
 
 function insertShiftArray(arr,num) {
     
     let newArr = [];
-
     for (let i = 0; i < arr.length; i++) {
 
-    newArr.push(arr[i]);
-        
-
       if(Math.ceil(arr.length/2) == i){
-        
-        
+
         newArr.push(num)
-        
         }
+         newArr.push(arr[i]);
     }
 
-console.log(newArr)
   return newArr;
 }
 
-insertShiftArray(arr,num);
+
+
+module.exports = {
+    array:insertShiftArray(arr,num)
+}
