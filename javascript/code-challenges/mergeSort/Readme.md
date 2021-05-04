f015d6b75ba1022e5b51ada6ffbd7bd16b9d4d66
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-05-04 10:56:05 +0300

    whitboard

---
+++ javascript/code-challenges/mergeSort/Readme.md
@@ -0,0 +1,17 @@
+# Challenge Summary
+
+- Merge Sort is a Divide and Conquer algorithm. It divides the input array into two halves then  merging two halves
+
+## Challenge Description
+
+- merge sort array to do it in less time
+
+## Approach & Efficiency
+
+- to make it less complex as possible .
+- space: O(n)
+- time: O(log n)
+
+## Solution
+
+![Image](../assats/code27.png)
--- javascript/code-challenges/mergeSort/mergeSort.test.js
+++ javascript/code-challenges/mergeSort/mergeSort.test.js
@@ -1,7 +1,11 @@
 const megaSort = require("./mergeSort");
 describe("megaSort", () => {
-  test("Happy Path return an array sorted successfully", () => {
+  test("Happy Path ", () => {
     let arr = [14, 16, 22, 16, 15, 2, 84, 6];
-    expect(megaSort(arr)).toStrictEqual("s");
+    expect(megaSort(arr)).toStrictEqual([2, 6, 14, 15, 16, 16, 22, 84]);
+  });
+  test("should return empty array ", () => {
+    let arr = [];
+    expect(megaSort(arr)).toStrictEqual([]);
   });
 });
